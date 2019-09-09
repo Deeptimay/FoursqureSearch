a58945d0d63117d7e0aba4d4d5bb5b8fdb5c9795
unknown <deeptimayroutray@gmail.com>
2019-09-09 22:08:45 +0530

    keystore added

--- app/src/main/java/com/deeptimay/yuluvenues/activity/MainActivity.java
+++ app/src/main/java/com/deeptimay/yuluvenues/activity/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(i);
                 } else {
                     // Notifies the user if there are insufficient location permissions
-                    Toast.makeText(getApplicationContext(), "Mr. Jitters is missing permissions to access your location!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Missing permissions to access your location!", Toast.LENGTH_LONG).show();
                 }
             }
         });
